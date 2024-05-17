310a509a1bef9f6109aae4b0474dc94db0df9d0e
Aleksey Petunin <115166748+Alexfuz59@users.noreply.github.com>
2024-05-17 10:47:54 +0300

    added payment_key validation in the scheme

--- scheme/order.py
+++ scheme/order.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+import re
+from pydantic import BaseModel, field_validator
 from enums.order_enums import Status
 
 
@@ -7,3 +8,10 @@ class Order(BaseModel):
     payment_key: str
     status: Status
     total_price: int
+
+    @field_validator('payment_key')
+    def check_payment_key(cls, payment_key):
+        pattern = r'cs_test_[\d\w]+'
+        payment_key_result = re.fullmatch(pattern, payment_key)
+        if not payment_key_result:
+            raise ValueError('Invalid payment_key')
